559b05e2c1d3dde5c0e8bd554420e3b9aeadff6e
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2023-03-24 16:55:01 -0400

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats= ["Milo" , "Otis" , "Garfield"]; //create array 'cats' with names
+
+function destructivelyAppendCat(){  //create funct 'destructivelyAppendCat() with no arguemnt needed
+    cats.push("Ralph");             //push 'Ralp' to end of array
+}
+
+function destructivelyPrependCat(){ //create func 'destructivelyPrependCat()
+    cats.unshift("Bob");            //add 'Bob' to beginning of array
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(catCopy){
+    catCopy=[...cats,"Broom"];
+    return catCopy;
+}
+
+function prependCat(catCopy){
+    catCopy=["Arnold",...cats];
+    return catCopy;
+}
+
+function removeLastCat(){
+    return cats.slice(0,-1);
+}
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
